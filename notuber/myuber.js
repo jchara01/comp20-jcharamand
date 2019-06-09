5e4165b4a5b8359ddee2c631884451d0000dcd76
Judy Charamand <50532223+jchara01@users.noreply.github.com>
2019-06-09 15:05:28 -0400

    version 6

--- notuber/myuber.js
+++ notuber/myuber.js
@@ -1,10 +1,11 @@
-var map;
-function initMap() {
-    var SouthStation = {lat: 42.352271, lng: -71.05524200000001};
-    var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 8, center: SouthStation})
-    var marker = new google.maps.Marker({position: SouthStation, map: map});
-}
-
-async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_IfcaaMVOEL4s8tVHPA6K5C4gD3joeTo&callback=initMap" ;
-
+<script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 42.352271, lng: -71.05524200000001},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_IfcaaMVOEL4s8tVHPA6K5C4gD3joeTo&callback=initMap"
+    async defer></script>
