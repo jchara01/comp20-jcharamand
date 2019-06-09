4d2196e8118d3dbebaf1ea0e523b8e2c76897dae
Judy Charamand <50532223+jchara01@users.noreply.github.com>
2019-06-09 13:42:26 -0400

    version 2

--- notuber/myuber.js
+++ notuber/myuber.js
@@ -1,1 +1,10 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 42.352271, lng: -71.05524200000001},
+          zoom: 8
+        });
+      }
 
+src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer
