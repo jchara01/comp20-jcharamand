bb93a8f2bfe86c57c1c84536053153c69ac114d5
Judy Charamand <50532223+jchara01@users.noreply.github.com>
2019-06-09 17:21:44 -0400

    version 11

--- notuber/myuber.js
+++ notuber/myuber.js
@@ -11,4 +11,26 @@ function initMap() {
         });
 
         document.add
+  
+        var icons = {
+          car: {
+          icon: 'car.png';
+          }
+        };
+
+        var features = [
+        {
+                position: new google.maps.LatLng(42.3453, -71.0464)
+                type: 'car';
+        }
+        
+          
+        for (var i = 0; i < features.length; i++) {
+          var marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+          });
+        };
+
 }
