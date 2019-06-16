0246fd6660eeb184c7c7c637e27db0e3c2ceb466
Judy Charamand <50532223+jchara01@users.noreply.github.com>
2019-06-16 14:25:17 -0400

    added calculateDistance function

--- notuber/myuber.js
+++ notuber/myuber.js
@@ -61,6 +61,22 @@ function initMap(vehicles) {
          	vehicles.push(marker);
           	vehicles[i].setMap(map);
 	}
+}
 
+function calculateDistance(vehicleLat, vehicleLng) {
+	ar R = 6371e3; // metres
+	var lat1 = userLat.toRadians();
+	var lat2 = vehicleLat.toRadians();
+	var lon1 = userLng.toRadians();
+	var lon2 = vehicleLng.toRadians;
+	
+	var Δφ = (lat2-lat1).toRadians();
+	var Δλ = (lon2-lon1).toRadians();
 
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        	Math.cos(lat1) * Math.cos(lat2) *
+        	Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	var d = R * c;
 }
